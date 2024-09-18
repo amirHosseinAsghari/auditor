92d8d81b941b2d34fba759f8675c6f52ddda0794
Amir Hossein Asghari <amir.hossein.asghari.x@gmail.com>
2024-09-18 22:26:15 +0330

    Vini

--- src/api/axiosConfig.ts
+++ src/api/axiosConfig.ts
@@ -1,41 +1,41 @@
-import { showErrorToast } from "@/utils/toast";
+import {showErrorToast} from "@/utils/toast";
 import axios from "axios";
+import {toast} from "sonner";
 
+let toastId: any = null;
 const axiosInstance = axios.create({
-  // TODO
-  baseURL: "http://127.0.0.1:8000/api",
-  timeout: 10000,
-  headers: {
-    "Content-Type": "application/json",
-  },
+    // TODO
+    baseURL: "http://127.0.0.1:8000/api",
+    timeout: 10000,
+    headers: {
+        "Content-Type": "application/json",
+    },
 });
 
 axiosInstance.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("token");
-    if (token) {
-      // TODO set custom header
-      config.headers.Authorization = token;
+    (config) => {
+        const token = localStorage.getItem("token");
+        if (token) {
+            // TODO set custom header
+            config.headers.Authorization = token;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
     }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
 );
 
 axiosInstance.interceptors.response.use(
-  (response) => {
-    return response;
-  },
-  (error) => {
-    if (axios.isAxiosError(error) && error.response) {
-      showErrorToast("An error occurred");
-      return Promise.reject(error.response);
+    (config) => {
+        if (config.method === "post" || config.method === "patch" || config.method === "put" || config.method === "delete"){
+            toastId = toast.loading("Loading")
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
     }
-
-    return Promise.reject(error);
-  }
 );
 
 export default axiosInstance;
